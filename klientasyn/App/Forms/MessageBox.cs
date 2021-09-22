0e4191a95ea359999142660c12aee96644a1bc6f
Maniek13 <mani3k1989@gmail.com>
2021-09-22 23:56:27 +0200

    code

--- klientasyn/App/Forms/MessageBox.cs
+++ klientasyn/App/Forms/MessageBox.cs
@@ -80,13 +80,14 @@ namespace Klient
         {
             while (stop == false)
             {
-                if (DateTime.Now.Second % 3 == 0)
+                if (DateTime.Now.Second % 2 == 0)
                 {
                     var msg = PrivChatEVT.chatEVTDatas.Find(el => el.Name == filename);
                     msg.Msg = "Wyswietl wiadomosci#" + filename + "%" + Account.usser;
                     Wiadomości();
                     msg.msgsShowed.WaitOne();
                     msg.msgsShowed.Reset();
+                    msg.msgsShowed.WaitOne(1000);
                 }
             }
         }
